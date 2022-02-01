698398f0097596bc841e59366216e5f636a84de9
jensyg <jensysystec@gmail.com>
2022-02-01 07:10:51 -0300

    Create Ajustes_Iniciais.sh

---
+++ Ajustes_Iniciais.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#FUNÇÃO DE CREAR EL LOGO DE LA EMPRESA
+Logo_Empresa(){
+	clear
+		echo -e " \e[1;31m ======================================================================== \e[m ";
+		figlet -c "$EMPRESA"
+		echo -e " \e[1;31m ======================================================================== \e[m ";
+		echo ""
+		echo ""
+	return
+}
+
+
+clear
+echo -e "carregando..."
+sleep 1
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND
+EMPRESA=SYSTEC
+pacote=$(dpkg --get-selections | grep "figlet" )
+	if [ -n "$pacote" ] ;then
+		echo
+	else
+		apt-get install figlet -qq > /dev/null
+	fi
+
+
+
+clear
+
+echo""
+echo""
+echo -e " \e[1;31m ======================================================================== \e[m ";
+echo ""
+Logo_Empresa
+echo ""
+echo -e " \e[1;31m ======================================================================== \e[m ";
+echo ""
+echo ""
+
